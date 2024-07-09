384a69bfbc97675196cea8d5d2289bb84f699f76
abiflex <102967800+abiflex@users.noreply.github.com>
2024-07-09 10:36:36 +0000

    Log in screen implementation

---
+++ Login Screem/Login Screem/Form2.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Login_Screem
+{
+    public partial class Form2 : Form
+    {
+        public string username { get; set; }
+        public string password { get; set; }
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            label1.Text = Name;
+            label2.Text = password;
+        }
+    }
+}
